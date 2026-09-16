d912d8511610d20d4ec0d906a90ad66b341c6018
Sowmiya <24cs072@acetcbe.edu.in>
2026-09-16 19:04:22 +0530

    Add traffic density calculation

--- src/carbon_monitor.py
+++ src/carbon_monitor.py
@@ -12,3 +12,15 @@ carbon_emission = calculate_carbon_emission(
 print("EcoTwin Carbon Monitoring")
 print("Vehicles:", vehicles)
 print("Estimated Carbon Emission:", carbon_emission, "kg")
+def calculate_traffic_density(vehicle_count, road_capacity):
+    density = (vehicle_count / road_capacity) * 100
+    return density
+
+
+road_capacity = 200
+
+traffic_density = calculate_traffic_density(
+    vehicles, road_capacity
+)
+
+print("Traffic Density:", traffic_density, "%")
