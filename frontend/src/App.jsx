1d6661597f839858cd32cb6d31145465b36334c8
Sowmiya <24cs072@acetcbe.edu.in>
2026-09-18 19:03:36 +0530

    Develop EcoTwin frontend dashboard UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="dashboard">
+      <h1>EcoTwin Dashboard</h1>
+      <h2>Urban Carbon Dispersion Monitoring System</h2>
+
+      <div className="cards">
+        <div className="card">
+          <h3>Traffic Monitoring</h3>
+          <p>Vehicles: 150</p>
+          <p>Road Capacity: 200</p>
+          <p className="high">Traffic Level: High</p>
+          <p>Traffic Density:75%</p>
+        </div>
+
+        <div className="card">
+          <h3>Carbon Emission</h3>
+          <p>Total Emission: 18 kg</p>
+          <p className="moderate">Emission Level: Moderate</p>
+        </div>
+
+        <div className="card">
+          <h3>Traffic Optimization</h3>
+          <p>Recommended Action:</p>
+          <p className="action">Increase Green Signal Time</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
